6895d7c75ddc54bbb68e07b22eeb82b4b2e61d1e
kuechlerrobin <robbyfix3000@gmail.com>
2022-03-22 10:33:29 +0100

    Optimierung: Collision entfernt

--- src/main/java/de/gsso/sw_alpha/objects/Ground.java
+++ src/main/java/de/gsso/sw_alpha/objects/Ground.java
@@ -1,10 +1,17 @@
 package de.gsso.sw_alpha.objects;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
-public class Ground extends Collision {
+public class Ground extends ImageView {
+    private final Pane canvas;
 
     public Ground(Pane canvas, String name, double x, double y) {
-        super(canvas, "Img/Ground/"+name+".png", x, y);
+        super(new Image("Img/Ground/"+name+".png"));
+        this.canvas = canvas;
+        this.setY(y);
+        this.setX(x);
+        this.canvas.getChildren().add(this);
     }
 }
--- src/main/java/de/gsso/sw_alpha/objects/Spikes.java
+++ src/main/java/de/gsso/sw_alpha/objects/Spikes.java
@@ -1,9 +1,16 @@
 package de.gsso.sw_alpha.objects;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
-public class Spikes extends Collision{
+public class Spikes extends ImageView {
+    private final Pane canvas;
+
     public Spikes(Pane canvas, double x, double y) {
-        super(canvas, "Img/Demo/spikes.png", x, y);
+        super(new Image("Img/Demo/spikes.png"));
+        this.canvas = canvas;
+        this.setX(x);
+        this.setY(y);
     }
 }
